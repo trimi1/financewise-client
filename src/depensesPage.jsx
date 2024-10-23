98adea6ce35e4beb169c1a06a98d9bb0df788d61
Manca Mirko <m.manca@student.helmo.be>
2024-10-24 00:18:43 +0200

    MERGE

--- src/depensesPage.jsx
+++ src/depensesPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function DepensesPage() {
+
     const depenses = fetch(`http://localhost:8080/financewise/depenses/users/${localStorage.getItem("EMAIL")}`, {method: 'GET',  headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem("TOKEN")}`}})
         .then(response => response.json())
     console.log(depenses)
