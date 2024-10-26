37a33b3fcfba5b42ebc4f88db3d8d4f991e60f2d
Leutrim <l.ahmetaj@student.helmo.be>
2024-10-26 21:34:28 +0200

    categories.jsx component and categoriesPage.jsx page created + adaptation in main.jx to include the root of the categoriesPage.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import AuthPage from './page/authPage.jsx';
 import HomePage from './page/homePage.jsx';
 import GoalsPage from './page/goalsPage.jsx';
 import DepensesPage from "./page/depensesPage.jsx";
+import CategoriesPage from "./page/categoriesPage.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -13,7 +14,7 @@ createRoot(document.getElementById('root')).render(
                 <Route path="/" element={<AuthPage />} />
                 <Route path="/home" element={<HomePage />} />
                 <Route path="/depenses" element={<DepensesPage />} />
-                <Route path="/category" />
+                <Route path="/category" element={<CategoriesPage />} />
                 <Route path="/goals" element={<GoalsPage />} />
                 <Route path="/investments" />
                 <Route path="/quizz" />
---
+++ src/page/categoriesPage.jsx
@@ -0,0 +1,16 @@
+import Header from "../component/header.jsx";
+import Aside from "../component/aside.jsx";
+import Categories from "../component/categories.jsx";
+
+function CategoriesPage() {
+    return (<>
+     <Header />
+     <main className="is-flex is-flex-direction-row">
+        <Aside />
+        <Categories />
+     </main>
+    </>
+    );
+}
+
+export default CategoriesPage;
